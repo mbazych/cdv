7255f8490cef65d2ccfb811dd57d4fb8c37f7218
mbazych <mbazych@edu.cdv.pl>
2019-04-16 15:43:08 +0200

    :)

---
+++ listy_tuple_slowniki.py
@@ -0,0 +1,14 @@
+# listy
+imiona = ['Julia', 'Anna', 'Krystyna']
+print(type(imiona))
+imiona.append('Janusz')
+ilosc = len(imiona)
+print(f'Ilość imion:  {ilosc}')
+
+# tuple
+nazwiska = ('Kowalski', 'Nowak')
+print(type(nazwiska))
+print(nazwiska)
+# tuple szybsze, ale nic sie nie dodaje
+ilosc = len(nazwiska)
+print(f'Ilość nazwisk: {ilosc}')
